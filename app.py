5d1bce5b4b76412543fcd265806ce4040eeee39c
mateuspgalmeida-ctrl <mateuspgalmeida@gmail.com>
2026-03-25 15:17:14 -0300

    app.py

--- app.py
+++ app.py
@@ -142,8 +142,8 @@ def main():
         if menu == "🏠 Início":
             st.markdown(f'''
                 <div class="moldura-3d">
-                    <h1 class="titulo-principal">Janeide Xavier LTDA</h1>
-                    <p class="subtitulo-principal">Transformando dados em lares em Feira de Santana</p>
+                    <h1 class="titulo-principal">Imobiliária Janeide Xavier LTDA</h1>
+                    <p class="subtitulo-principal">Inteligência imobiliária em Feira de Santana</p>
                 </div>
             ''', unsafe_allow_html=True)
             
